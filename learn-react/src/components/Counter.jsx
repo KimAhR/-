a3c7213024030b776d545743e70efeb185776bf0
KimAhR <122251792+KimAhR@users.noreply.github.com>
2023-01-26 21:01:40 +0900

    Update Counter.jsx

--- learn-react/src/components/Counter.jsx
+++ learn-react/src/components/Counter.jsx
@@ -6,7 +6,12 @@ function Counter() {
   // 상태값이 업데이트되면 재랜더링이 일어난다.
   // => 이벤트 바인딩시 인자를 전달해야하면 익명함수 내에서 호출하는 형태로 전달해야한다.
   const handleCount = (operend) => {
-    //상태  업데이트는 비동기적으로 동작한다.
+    /* 
+        상태  업데이트는 비동기적으로 동작한다.
+            => 함수형 업데이트를 하면 동기적으로 동작할 수 있다.
+            => 업데이트 함수에 함수를 전달하면 매개변수로 최신 상태값이 전달된다.
+            => 디펜던시를 비울 때 사용하면 좋다.
+    */
     setCount((num) => num + operend);
     setCount((num) => num + operend);
     setCount((num) => num + operend);
